da752bf06062d17686db610a36bfd51879878507
Cristian Roque <eucristianroque@gmail.com>
2025-03-07 12:00:47 -0300

    quartoCommit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,2 @@
-const headerLinks = document.querySelector("#headerLinks");
-const options = document.querySelector("#options");
-const xHeader = document.querySelector("#xHeader")
 
-options.addEventListener("click", () => {
-    headerLinks.style.animation = "aparecer 1.2s forwards"
-    headerLinks.style.position = "fixed"
-})
 
-xHeader.addEventListener("click", () => {
-    headerLinks.style.animation = "none"
-})
